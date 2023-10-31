5b305741913ea57986c2d24cad2c268f612f3447
Resn4R <purview-joust.0x@icloud.com>
2023-10-31 16:09:38 +0000

    Initial Commit

---
+++ Leap Year TDD/Leap_Year_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leap_Year_TDDApp.swift
+//  Leap Year TDD
+//
+//  Created by Vito Borghi on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Leap_Year_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
