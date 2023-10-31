46d25874051c2180a5e007a51730ddc1527b957b
Resn4R <purview-joust.0x@icloud.com>
2023-10-31 16:16:06 +0000

    first commit

--- Leap Year TDDTests/Leap_Year_TDDTests.swift
+++ Leap Year TDDTests/Leap_Year_TDDTests.swift
@@ -9,28 +9,10 @@ import XCTest
 @testable import Leap_Year_TDD
 
 final class Leap_Year_TDDTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    //Write a function that returns true or false depending on whether its input integer is a leap year or not.
+    //A leap year is divisible by 4 but is not otherwise divisible by 100 unless it is also divisible by 400.
+    func testGivenYearIs2077WhenAskedIfLeapYearThenReturnsFalse() {
+        let sample = Year(value: 2077)
+        XCTAssertFalse(sample.isLeapYear())
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
---
+++ Year.swift
@@ -0,0 +1,19 @@
+//
+//  Year.swift
+//  Leap Year TDD
+//
+//  Created by Vito Borghi on 31/10/2023.
+//
+
+import Foundation
+
+class Year {
+    let value: Int
+    func isLeapYear() -> Bool {
+        false
+    }
+    
+    init(value: Int){
+        self.value = value
+    }
+}
